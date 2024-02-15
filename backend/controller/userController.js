2011779ac9fbf844f6c264345576d7b807c73f65
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2024-02-15 16:46:32 +0530

    Authentication

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,7 @@
 import asyncHandler from "../middilewares/asyncHandler.js";
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
-import createToken from "../utils/createToken.js"
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
@@ -15,7 +15,7 @@ const createUser = asyncHandler(async (req, res) => {
   const userExists = await User.findOne({ email });
   if (userExists) res.status(400).send("Email Already Registered");
 
-/* 
+  /* 
 What is Salt?
 Ans:In simpler terms, a "salt" is a randomly generated value used to add complexity and uniqueness to a hashed password. 
 It's like adding a secret ingredient to a recipe that makes it harder for someone to guess the original password. 
@@ -34,7 +34,7 @@ like pre-computed rainbow table attacks.
   try {
     await newUser.save();
     // Cookies Creation
-    createToken(res,newUser._id)
+    createToken(res, newUser._id);
 
     res.status(201).json({
       _id: newUser._id,
@@ -48,15 +48,18 @@ like pre-computed rainbow table attacks.
   }
 });
 
-const loginUser = asyncHandler(async(req,res)=>{
-  const{email, password}=req.body;
-  
-  const existingUser= await User.findOne({email})
-  if(existingUser){
-    const isPasswordValid=await bcrypt.compare(password, existingUser.password)
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-    if(isPasswordValid){
-      createToken(res,existingUser._id)
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (isPasswordValid) {
+      createToken(res, existingUser._id);
 
       res.status(201).json({
         _id: existingUser._id,
@@ -67,21 +70,68 @@ const loginUser = asyncHandler(async(req,res)=>{
       return;
     }
   }
+});
 
-})
-
-const logoutCurrentUser=asyncHandler(async(req,res)=>{
-  res.cookie('jwt','',{
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
     httpOnly: true,
-    expires: new Date(0)
-  })
+    expires: new Date(0),
+  });
 
-  res.status(200).json({message:"Log Out Succesfully"})
-})
+  res.status(200).json({ message: "Log Out Succesfully" });
+});
 
-const getAllUsers=asyncHandler(async(req,res)=>{
-  const users= await User.find({});
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
   res.json(users);
-})
+});
 
-export { createUser, loginUser ,logoutCurrentUser,getAllUsers};
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(405);
+    throw new Error("User not found !!");
+  }
+});
+
+const updateCurrentUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      user.password = hashedPassword;
+    }
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new error("User not found");
+  }
+});
+
+export {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUser,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,29 @@
 import express from "express";
-import {createUser ,loginUser,logoutCurrentUser,getAllUsers} from "../controller/userController.js";
-import {authenticate,authorizeAdmin} from "../middilewares/authMiddleware.js";
+import {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUser,
+} from "../controller/userController.js";
+import {
+  authenticate,
+  authorizeAdmin,
+} from "../middilewares/authMiddleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/').post(createUser).get(authenticate,authorizeAdmin,getAllUsers);
-router.post('/auth', loginUser);
-router.post('/logout', logoutCurrentUser);
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizeAdmin, getAllUsers);
+router.post("/auth", loginUser);
+router.post("/logout", logoutCurrentUser);
 
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUser);
 
 export default router;
