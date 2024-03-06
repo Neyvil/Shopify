353412d544305f7e6674ccfbbe41d437120bede5
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2024-03-07 01:15:04 +0530

    Admin backend

--- backend/controller/categoryController.js
+++ backend/controller/categoryController.js
@@ -1,14 +1,46 @@
 import Category from "../models/categoryModel.js";
 import asyncHandler from "../middilewares/asyncHandler.js";
+import res from "express/lib/response.js";
 
 const createCategory = asyncHandler(async (req, res) => {
-    try {
-        const { name } = req.body;
-        console.log(name);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json(error);
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.json({ error: "Name is required" });
     }
+
+    const existingCategory = await Category.findOne({ name });
+
+    if (existingCategory) {
+      return res.json({ error: "Already exists" });
+    }
+
+    const category = await new Category({ name }).save();
+    res.json(category);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
 });
 
-export { createCategory };
+const updateCategory = asyncHandler(async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { categoryId } = req.params;
+
+    const category = await Category.findOne({ _id: categoryId });
+
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    category.name = name;
+    const updatedCategory = await category.save();
+    res.json(updatedCategory);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+export { createCategory, updateCategory };
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { createCategory } from "../controller/categoryController.js";
+import {
+  createCategory,
+  updateCategory,
+} from "../controller/categoryController.js";
 const router = express.Router();
-
 
 import {
   authenticate,
   authorizeAdmin,
 } from "../middilewares/authMiddleware.js";
 
-router.route("/").post(createCategory);
+router.route("/").post(authenticate, authorizeAdmin, createCategory);
+router.route("/:categoryId").put(authenticate, authorizeAdmin, updateCategory);
 
 export default router;
