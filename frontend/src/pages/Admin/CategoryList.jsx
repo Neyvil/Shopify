5913e7f05f890186d3b132a312322b621028fc88
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2024-03-10 01:25:41 +0530

    Admin backend

--- frontend/src/pages/Admin/CategoryList.jsx
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -19,6 +19,27 @@ const CategoryList = () => {
   const [updateCategory] = useUpdateCategoryMutation();
   const [deleteCategory] = useDeleteCategoryMutation();
 
+  const handleCreateCategory= async (e) =>{
+    e.preventDefault()
+
+    if(!name){
+      toast.error('Category name is required')
+      return
+    }
+    try {
+      const result=await createCategory({name}).unwrap()
+      if (result.error){
+        toast.error(result.error)
+      }else{
+        setName("")
+        toast.success(`${result.name} is created`)
+      }
+      
+    } catch (error) {
+      console.error(error)
+      toast.error("creating category failed, try again")
+    }
+  }
 
   return (
     <div className=" ml-[10rem] flex flex-col md:flex-row">
@@ -27,15 +48,22 @@ const CategoryList = () => {
         <div className="h-12 text-white font-bold text-3xl">
           Manage Categories
         </div>
-        <CategoryForm value={name} setValue={setName}/>
+        <CategoryForm value={name} setValue={setName} handleSubmit={handleCreateCategory}/>
         <br />
         <hr />
 
         <div className="flex flex-wrap">
-          {categories?.map((category)=>(
+          {categories?.map((category) => (
             <div key={category._id}>
-              <button className="bg-white border border-pink-500 text-500 py-2 px-4 rounded-lg m-3 hover:bg-pink-500 hover:text-white focus:text-white focus:outline-none focus:ring-pink-500 ">
-
+              <button
+                className=" border border-pink-500 text-500 py-2 px-4 rounded-lg m-3 hover:bg-pink-500 hover:text-white focus:text-white focus:outline-none focus:ring-pink-500 focus:ring-opacity-50"
+                onClick={() => {
+                  setModalVisible(true);
+                  setSelectedCategory(category);
+                  setUpdateName(category.name);
+                }}
+              >
+                {category.name}
               </button>
             </div>
           ))}
